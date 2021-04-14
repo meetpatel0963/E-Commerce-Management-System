25340700f6cc3327a17c8d92b1ce67c6a72c070d
MEET PATEL <meetpatel0963@gmail.com>
2021-04-15 00:31:01 +0530

    Add files via upload

---
+++ config/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+    $conn=mysqli_connect('localhost','cheris','cherisom13102000','Shopping');
+    if (!$conn) {
+        echo 'Connection Error : '. mysqli_connect_error(); 
+    }
+    else{
+    }
+
+?>
