fd6146d742fc8a7718af2ba30a62f4d2c7b813a6
MEET PATEL <meetpatel0963@gmail.com>
2021-04-15 00:29:53 +0530

    Delete dbconnect.php

--- config/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn=mysqli_connect('localhost','cheris','cherisom13102000','Shopping');
-    if (!$conn) {
-        echo 'Connection Error : '. mysqli_connect_error(); 
-    }
-    else{
-    }
-
-?>
